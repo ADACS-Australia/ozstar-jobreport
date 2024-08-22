a4b4712d8e1aed78ff4721ea179b8f70c55b743f
David Liptai <dliptai@swin.edu.au>
2024-08-22 15:12:13 +1000

    epilog: always exit with success so we don't accidentally drain the node

--- slurmctldepilog.sh
+++ slurmctldepilog.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # SLURM: slurmctld epilog run by the control daemon on transom1
+# Always exit with success so we don't accidentally drain the node
 
 if [ -z "${SLURM_JOB_ID}" ]; then
         echo "This script can only by run by the Slurm software"
-        exit 1
+        exit 0
 fi
 
 # Run jobsummary for the job ID
-/apps/jobsummary/jobsummary "$SLURM_JOB_ID"
-
-# Always exit with success so we don't accidentally kill the job
+/apps/jobsummary/jobsummary "$SLURM_JOB_ID" || true
 exit 0
