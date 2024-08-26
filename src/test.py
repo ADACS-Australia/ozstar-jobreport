73ae0db283c46d86189a2ac577c934df3ad72177
David Liptai <dliptai@swin.edu.au>
2024-08-26 10:42:49 +1000

    fix up test

--- src/test.py
+++ src/test.py
@@ -5,15 +5,26 @@ import pyslurm
 from jobsummary import get_summary
 from tqdm import tqdm
 
+def str_to_bool(value):
+    return value.lower() in {"true", "1", "yes", "y"}
+
+VERBOSE = str_to_bool(os.environ.get("VERBOSE", "no"))
+DEBUG = str_to_bool(os.environ.get("DEBUG", "no"))
+
+# Get current file path
+fpath = os.path.abspath(__file__)
+
 # Get a list of jobs from squeue
 test_jobs = pyslurm.Jobs.load()
 
 for job in tqdm(test_jobs, desc="Testing jobs"):
     # Suppress print
-    sys.stdout = open(os.devnull, "w")
+    if not VERBOSE:
+        sys.stdout = open(os.devnull, "w")
 
     # Test job
-    print(get_summary(job))
+    print(get_summary(job, influx_config=f"{fpath}/../conf.influxdb.toml"), DEBUG)
 
     # Restore print
-    sys.stdout = sys.__stdout__
+    if not VERBOSE:
+        sys.stdout = sys.__stdout__
